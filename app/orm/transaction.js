81412df9a68b537c711f4718024cb4e0a86b4655
rehmanShafi <rehman@maher.pk>
2016-11-17 18:56:19 +0500

    Sub Total over reciept

--- app/orm/transaction.js
+++ app/orm/transaction.js
@@ -1,4 +1,3 @@
-//console.log(J2T);
 import _ from 'lodash';
 
 let Transaction = new PG.Table('transactions');
@@ -24,6 +23,7 @@ Meteor.methods({
     let printer = new Escpos.Printer(device);
     let time = new Date();
     let obj = {};
+    let subTotal = 0;
     obj.DATE = time;
     obj.ITEMS = data.items;
     obj.SalesTax = parseFloat(data.trans.properties.tax).toFixed(2)
@@ -42,30 +42,34 @@ Meteor.methods({
         .style('normal')
         .text('_'.repeat(48))
         .size(1, 1)
-        let head = 'NAME' + 'QTY'+' '.repeat(3)+'PRICE';
-        printer
-        .text('NAME'+' '.repeat(48-head.length)+'QTY'+' '.repeat(3)+'PRICE')
+      let head = 'NAME' + 'QTY' + ' '.repeat(3) + 'PRICE';
+      printer
+        .text('NAME' + ' '.repeat(48 - head.length) + 'QTY' + ' '.repeat(3) + 'PRICE')
         .text('='.repeat(48))
-        _.forEach(obj.ITEMS, (item) =>{
-            let string = item.Quantity + " ".repeat(4-String(item.Quantity).length) + item.Name +' '.repeat(5-String(item.Price).length) + item.Price + '.00';
-            let spaced = item.Name + " ".repeat(48 - string.length) + " ".repeat(3-String(item.Quantity).length) + item.Quantity + " ".repeat(5-String(item.Price).length) + item.Price + '.00';
-            printer
-            .align('ct')
-            .text(spaced)
-        });
+      _.forEach(obj.ITEMS, (item) => {
+        let string = item.Quantity + " ".repeat(4 - String(item.Quantity).length) + item.Name + ' '.repeat(5 - String(item.Price).length) + item.Price + '.00';
+        let spaced = item.Name + " ".repeat(48 - string.length) + " ".repeat(3 - String(item.Quantity).length) + item.Quantity + " ".repeat(5 - String(item.Price).length) + item.Price + '.00';
+        subTotal += parseInt(item.Price);
         printer
+          .align('ct')
+          .text(spaced)
+      });
+      let sB = 'Sub Total' + subTotal + '.00';
+      let sBSpaced = 'Sub Total' + ' '.repeat(48-sB.length) + subTotal + '.00';
+      printer
         .align('ct')
         .text('-'.repeat(48))
         .control('LF')
-        let sT = 'Sales Tax' + obj.SalesTax;
-        let sTSpaced = 'Sales Tax' + " ".repeat(48-sT.length) + obj.SalesTax;
-        printer
+        .text(sBSpaced)
+      let sT = 'Sales Tax' + obj.SalesTax;
+      let sTSpaced = 'Sales Tax' + " ".repeat(48 - sT.length) + obj.SalesTax;
+      printer
         .text(sTSpaced)
         .size(2, 2)
         .control('LF')
-        let tO = 'Total' + obj.Total;
-        let tOSpaced = 'Total' + " ".repeat(48-tO.length) + obj.Total;
-        printer
+      let tO = 'Total' + obj.Total;
+      let tOSpaced = 'Total' + " ".repeat(48 - tO.length) + obj.Total;
+      printer
         .text(tOSpaced)
         .text('')
         .text('')
